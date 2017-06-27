971e2dcfe9ecef3cbfe2fecb062668c385ad10d9
lex <327282764@qq.com>
2017-06-27 22:46:05 +0800

    添加新的Activity

--- timeview/src/main/java/com/lextime2013/view/MainActivity.java
+++ timeview/src/main/java/com/lextime2013/view/MainActivity.java
@@ -1,12 +1,29 @@
 package com.lextime2013.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTime = (Button) findViewById(R.id.btn_time);
+        btnTime.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_time:
+                startActivity(new Intent(this, TimeViewActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
